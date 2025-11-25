c785a39ed5aa786550faa92e0e8ef36ce891ba87
Matan <matan.ljubas@3ofthem.eu>
2025-11-25 20:04:00 +0100

    Update scripts

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -10,6 +10,8 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 PROJECT_DIR="$SCRIPT_DIR/.."
 
+# Change current working directory.
 cd "$PROJECT_DIR" 
 
+# Generate the documentation to the `docs` directory.
 npm run docs
--- scripts/generate-typings.sh
+++ scripts/generate-typings.sh
@@ -10,10 +10,20 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 PROJECT_DIR="$SCRIPT_DIR/.."
 
+# Change current working directory.
 cd "$PROJECT_DIR/apps/demo" 
 
-# Generate Android typings.
-ns build android && ns typings android --aar "$PROJECT_DIR/packages/ia-sdk/platforms/android/ia_sdk.aar"
+# Remove any previously-existing typings.
+rm -rf "$PROJECT_DIR/packages/ia-sdk/typings"
 
-# Generate iOS typings.
-# ns build ios && ns typings ios 
+# Generate and move Android typings.
+ns clean
+ns build android
+ns typings android --aar "$PROJECT_DIR/packages/ia-sdk/platforms/android/ia_sdk.aar"
+mv -f "$PROJECT_DIR/apps/demo/typings/android/" "$PROJECT_DIR/packages/ia-sdk/typings/android/" 
+
+# Generate and move iOS typings.
+ns clean
+ns build ios
+ns typings ios 
+mv -f "$PROJECT_DIR/apps/demo/typings/ios/" "$PROJECT_DIR/packages/ia-sdk/typings/ios/" 
