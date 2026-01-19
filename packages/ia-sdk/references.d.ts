4076e26bdaf4defc04fcfc6253b949c84aa0c8bf
Matan Ljubas <mljubas@4ofthem.eu>
2026-01-19 14:45:30 +0100

    NativeScript library deploy version 2026.01.19.885

--- packages/ia-sdk/references.d.ts
+++ packages/ia-sdk/references.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="../../references.d.ts" />
+/// <reference path="../../typings/android/android-declarations.d.ts" />
+/// <reference path="../../typings/android/android.d.ts" />
+/// <reference path="../../typings/ios/arm64/objc!nsswiftsupport.d.ts" />
+/// <reference path="../../typings/ios/x86_64/objc!nsswiftsupport.d.ts" />
